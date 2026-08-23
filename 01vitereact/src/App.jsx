75c1488ed73a007cba39ee16ff49d3840cc8718f
NISHIT <nisht@NISHTs-MacBook.local>
2026-08-23 10:08:22 +0530

    React under the hood

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,11 @@
 import Chai from "./chai"
 
 function App(){
+  const Username = 'Nisit and Code'
   return (
     <><h1>Nisit and React with vite | NK</h1>
     <Chai />
+    <h1>{Username}</h1>
     </>
   )
 }
